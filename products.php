e63c9246e6472c2bf4fa665f9e06de3cebe83686
askarov32 <insar@lessons.kz>
2024-08-06 08:43:39 +0500

    update commit

--- products.php
+++ products.php
@@ -7,21 +7,20 @@ class Product {
     public $description;
     public $details;
 
-    public function __construct($id, $name, $price, $image, $description, $details) {
+    public function __construct($id, $name, $price, $image, $description) {
         $this->id = $id;
         $this->name = $name;
         $this->price = $price;
         $this->image = $image;
         $this->description = $description;
-        $this->details = $details;
     }
 
     public static function getAllProducts() {
         return [
-            new Product(1, 'Adidas Daily 3.0', 48990, 'assets/img/DSC_8900 1 (1).png', 'Описание товара', 'Детали товара'),
-            new Product(2, 'Adidas Campus', 74990, 'assets/img/DSC_9001.JPG', 'Описание товара', 'Детали товара'),
-            new Product(3, 'Adidas Stellasport Yvori', 46990, 'assets/img/DSC_8998.JPG', 'Описание товара', 'Детали товара'),
-            // Добавь остальные продукты
+            new Product(1, '     Daily 3.0', 48990, 'assets/img/DSC_8900 1 (1).png', 'Описание товара'),
+            new Product(2, 'Adidas Campus', 74990, 'assets/img/DSC_9001.JPG', 'Описание товара'),
+            new Product(3, 'Adidas Stellasport Yvori', 46990, 'assets/img/DSC_8998.JPG', 'Описание товара'),
+                // Добавь остальные продукты
         ];
     }
 }
